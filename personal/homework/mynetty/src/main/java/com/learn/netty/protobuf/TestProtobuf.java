b282681b2fbd83f81b632de1fc66077b46e25991
SimithHack <xiefuqiang95@gmail.com>
2019-07-30 23:18:03 +0800

    protobuf

---
+++ personal/homework/mynetty/src/main/java/com/learn/netty/protobuf/TestProtobuf.java
@@ -0,0 +1,17 @@
+package com.learn.netty.protobuf;
+
+public class TestProtobuf {
+    public static void main(String[] args) throws Exception {
+        DataInfo.Student stu = DataInfo.Student.newBuilder()
+                .setName("张三")
+                .setAddress("成都")
+                .setAge(12)
+                .build();
+        byte[] studentByte = stu.toByteArray();
+        DataInfo.Student stuCopy = DataInfo.Student.parseFrom(studentByte);
+        System.out.println(String.format("name = %s, age = %s, address = %s",
+                stuCopy.getName(),
+                stuCopy.getAge(),
+                stuCopy.getAddress()));
+    }
+}
